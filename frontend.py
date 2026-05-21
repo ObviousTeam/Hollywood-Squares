648f5891de4b0c2f1226e2ac1503f6f1b7385fad
ReadabilityLOL <125707933+ReadabilityLOL@users.noreply.github.com>
2026-05-21 02:06:18 +0000

    committed

--- frontend.py
+++ frontend.py
@@ -1,1 +1,31 @@
+import streamlit as st
+
+def check_win():
+    #stuff happens here
+    pass
+
+def popup():
+    #stuff happens here
+    check_win()
+    pass
+
+
+
+board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+]
+
+st.title("Celebrity squares")
+
+t = st.columns([1,1,1])
+
+
+for x in range(len(board)):
+    for y in range(0,len(board[x])):
+        with t[y]:
+            st.button(board[x][y],key=f"{x}{y}", on_click=popup)
+    st.html("</p>")
+
 
