e08549e3c766cb017c494d6118fe93a905403114
ReadabilityLOL <125707933+ReadabilityLOL@users.noreply.github.com>
2026-05-21 02:37:56 +0000

    added some updates

--- frontend.py
+++ frontend.py
@@ -3,11 +3,15 @@ import streamlit as st
 def check_win():
     #stuff happens here
     pass
-
-def popup():
+  
+def popup(x,y):
     #stuff happens here
+    board[x][y] = "x"
+    for x in range(len(board)):
+        for y in range(0,len(board[x])):
+            st.session_state[f"{x}{y}f"] = f"{board[x][y]}"
+    st.dialog(f"{board} Hello")
     check_win()
-    pass
 
 
 
@@ -21,11 +25,12 @@ st.title("Celebrity squares")
 
 t = st.columns([1,1,1])
 
-
 for x in range(len(board)):
     for y in range(0,len(board[x])):
         with t[y]:
-            st.button(board[x][y],key=f"{x}{y}", on_click=popup)
+            st.session_state[f"{x}{y}f"] = f"{board[x][y]}"
+            st.button(st.session_state[f"{x}{y}f"],key=f"{x}{y}", on_click=popup,args=(x,y))
     st.html("</p>")
+
 
 
